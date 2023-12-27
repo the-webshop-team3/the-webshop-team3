b188c9fac492d9aa21fa750aed12d9f3e5ba08cf
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2023-12-27 11:57:29 +0100

    ✨ open and close cart

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,25 @@
 import "./../scss/style.scss";
+
+function openPage(theButton: HTMLButtonElement, thePage: HTMLElement) {
+  theButton.addEventListener("click", () => {
+    thePage.classList.add("active");
+  });
+}
+
+function closePage(theButton: HTMLButtonElement, thePage: HTMLElement) {
+  theButton.addEventListener("click", () => {
+    thePage.classList.remove("active");
+  });
+}
+
+//öppna och stäng varukorg
+const openCartButton = document.querySelector(
+  ".main-wrapper__cart-button"
+) as HTMLButtonElement;
+const closeCartButton = document.querySelector(
+  ".cart__close-button"
+) as HTMLButtonElement;
+const cartContainer = document.querySelector(".cart") as HTMLElement;
+
+openPage(openCartButton, cartContainer);
+closePage(closeCartButton, cartContainer);
