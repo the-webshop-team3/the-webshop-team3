7d000c704953099e103859f4672843082a6b4f68
Christian buaka <Christian.buaka@hotmail.com>
2023-12-27 12:14:15 +0100

    Added a modal function

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,24 @@
 import "./../scss/style.scss";
+
+
+
+const buyButton = document.getElementById('modalButton') as HTMLButtonElement;
+buyButton.addEventListener('click', handlePurchase);
+
+function handlePurchase(event: Event) {
+
+event.preventDefault()
+  showPurchaseModal();
+}
+
+function showPurchaseModal() {
+  const modal = document.getElementById('purchaseModal') as HTMLDivElement;
+  modal.style.display = 'block';
+
+  const closeModalButton = document.getElementById('closeModalButton') as HTMLButtonElement;
+  closeModalButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+}
+
+
