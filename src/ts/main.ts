f40343c416c8466d3f83111d999e398aee81dee3
Patrik Arell <patrikarell@gmail.com>
2023-12-27 14:14:44 +0100

    :construction: show product on productpage

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,45 +1,111 @@
 import "./../scss/style.scss";
 import { Product } from "./models/Product";
 
-
 const products = [
-    new Product("Kungsgran","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7xjNHyxM5rjPWc3rV_EZYVqaG5OmAso6eFw&usqp=CAU","Medium",499,"001"),
-    new Product("Rödgran","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7xjNHyxM5rjPWc3rV_EZYVqaG5OmAso6eFw&usqp=CAU", "Medium",350,"002"),
-    new Product("Kungsgran","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7xjNHyxM5rjPWc3rV_EZYVqaG5OmAso6eFw&usqp=CAU","Stor",699,"003"),
-    new Product("Kungsgran","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7xjNHyxM5rjPWc3rV_EZYVqaG5OmAso6eFw&usqp=CAU","Liten",350,"004"),
-    new Product("Rödgran","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7xjNHyxM5rjPWc3rV_EZYVqaG5OmAso6eFw&usqp=CAU", "Liten",250,"005"),
-    new Product("Rödgran","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7xjNHyxM5rjPWc3rV_EZYVqaG5OmAso6eFw&usqp=CAU", "Stor",390,"006"),
+  new Product(
+    "Kungsgran",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7xjNHyxM5rjPWc3rV_EZYVqaG5OmAso6eFw&usqp=CAU",
+    "Medium",
+    499,
+    "Text om gran 1",
+    "001"
+  ),
+  new Product(
+    "Rödgran",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7xjNHyxM5rjPWc3rV_EZYVqaG5OmAso6eFw&usqp=CAU",
+    "Medium",
+    350,
+    "Text om gran 2",
+    "002"
+  ),
+  new Product(
+    "Kungsgran",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7xjNHyxM5rjPWc3rV_EZYVqaG5OmAso6eFw&usqp=CAU",
+    "Stor",
+    699,
+    "Text om gran 3",
+    "003"
+  ),
+  new Product(
+    "Kungsgran",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7xjNHyxM5rjPWc3rV_EZYVqaG5OmAso6eFw&usqp=CAU",
+    "Liten",
+    350,
+    "Text om gran 4",
+    "004"
+  ),
+  new Product(
+    "Rödgran",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7xjNHyxM5rjPWc3rV_EZYVqaG5OmAso6eFw&usqp=CAU",
+    "Liten",
+    250,
+    "Text om gran 5",
+    "005"
+  ),
+  new Product(
+    "Rödgran",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7xjNHyxM5rjPWc3rV_EZYVqaG5OmAso6eFw&usqp=CAU",
+    "Stor",
+    390,
+    "Text om gran 6",
+    "006"
+  ),
 ];
 
 console.log(products);
 
 const createProductsHtml = () => {
-for (let i = 0; i < products.length; i++){
-    const productContainer = document.createElement("div")
+  for (let i = 0; i < products.length; i++) {
+    const productContainer = document.createElement("div");
     const productTitle = document.createElement("h2");
     const productImage = document.createElement("img");
     const productPrice = document.createElement("p");
     const productID = document.createElement("p");
-    const productSize = document.createElement("p")
-    const addToCartButton = document.createElement("button")
+    const productSize = document.createElement("p");
+    const addToCartButton = document.createElement("button");
 
-  productTitle.innerHTML = products[i].title;
-  productImage.innerHTML = products[i].imageUrl;
-  productSize.innerHTML = products[i].size;
-  productPrice.innerHTML = products[i].price.toString();
-  productImage.setAttribute("src",products[i].imageUrl);
-  productID.innerHTML = "Art.nr: " + products[i].id
-  addToCartButton.innerHTML = "Lägg i varukorg"
+    productTitle.innerHTML = products[i].title;
+    productImage.innerHTML = products[i].imageUrl;
+    productSize.innerHTML = products[i].size;
+    productPrice.innerHTML = products[i].price.toString();
+    productImage.setAttribute("src", products[i].imageUrl);
+    productID.innerHTML = "Art.nr: " + products[i].id;
+    addToCartButton.innerHTML = "Lägg i varukorg";
 
-  productContainer.classList.add("main-wrapper__products__product-card")
+    productContainer.classList.add("main-wrapper__products__product-card");
 
-  productContainer.appendChild(productTitle)
-  productContainer.appendChild(productImage)
-  productContainer.appendChild(productSize)
-  productContainer.appendChild(productPrice)
-  productContainer.appendChild(addToCartButton)
-  document.querySelector(".main-wrapper__products")?.appendChild(productContainer)
-}
-}
-createProductsHtml()
+    productContainer.appendChild(productTitle);
+    productContainer.appendChild(productImage);
+    productContainer.appendChild(productSize);
+    productContainer.appendChild(productPrice);
+    productContainer.appendChild(addToCartButton);
+    document
+      .querySelector(".main-wrapper__products")
+      ?.appendChild(productContainer);
 
+    productContainer.addEventListener("click", () => {
+      const productPage = document.querySelector(".main-wrapper__product-page");
+      const productPageTitle = document.getElementById("product-page-title");
+      const productPageImage = document.getElementById("product-page-image");
+      const productPageInfo = document.getElementById("product-page-info");
+      const productPagePrice = document.getElementById("product-page-price");
+
+
+      productPage?.classList.add("main-wrapper__product-page--active");
+
+      if (productPageTitle) {
+        productPageTitle.innerHTML = products[i].title;
+      }
+      if (productPageImage) {
+        productPageImage.setAttribute("src",products[i].imageUrl)
+      }
+      if (productPageInfo) {
+        productPageInfo.innerHTML = products[i].info;
+      }
+      if (productPagePrice){
+        productPagePrice.innerHTML = products[i].price.toString() + " kr";
+      }
+    });
+  }
+};
+createProductsHtml();
